8101ceed64ea3ef713d03b7ae72e8329086c4a75
unknown <lauranuezzaragoza@gmail.com>
2020-04-12 18:53:04 +0200

    L2 servidor concurrente

--- servidor.c
+++ servidor.c
@@ -6,13 +6,124 @@
 #include <netinet/in.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <pthread.h>
+
+
+
+void *AtenderCliente (void *socket)
+{
+	int sock_conn;
+	int *s;
+	s= (int *) socket;
+	sock_conn= *s;
+	
+	char peticion[512];
+	char respuesta[512];
+	int ret;
+	
+	//Bucle de atencion al cliente
+	int terminar=0;
+	while (terminar ==0 )
+	{
+		// Ahora recibimos su nombre, que dejamos en buff
+		ret=read(sock_conn,peticion, sizeof(peticion));
+		printf ("Recibido\n");
+		
+		// Tenemos que a?adirle la marca de fin de string 
+		// para que no escriba lo que hay despues en el buffer
+		peticion[ret]='\0';
+		
+		//Escribimos el nombre en la consola
+		printf ("Peticion: %s\n",peticion);
+		
+		//Vamos a ver que quieren
+		char *p = strtok( peticion, "/");
+		int codigo =  atoi (p);
+		char nombre[20];
+		
+		if (codigo !=0)
+		{
+			p = strtok( NULL, "/");
+			strcpy (nombre, p);
+			printf ("Codigo: %d, Nombre: %s\n", codigo, nombre);
+		}
+		
+		if (codigo==0)
+		{
+			terminar =1;
+		}
+		
+		else if (codigo ==1) //piden la longitd del nombre
+			sprintf (respuesta,"%d", strlen(nombre));
+		
+		else if (codigo ==2 )
+		{
+			// quieren saber si el nombre es bonito
+			if((nombre[0]=='M') || (nombre[0]=='S'))
+			strcpy (respuesta,"SI");
+			else
+				strcpy (respuesta,"NO");
+		}
+			
+		else if(codigo==3) //Decir si es alto
+		{
+			p = strtok( NULL, "/");
+			float altura =  atof (p);
+			if (altura>1.70)
+				sprintf(respuesta, "%s: eres alto", nombre);
+			else
+				sprintf(respuesta, "%s: eres bajo", nombre);
+			
+		}
+		
+		else if(codigo == 4)
+		{
+			int NoIguales=0;
+			int i=0;
+			int l=strlen(nombre)-1;
+			while((NoIguales!=1) && (i<l))
+			{
+				if(nombre[i]!=nombre[l])
+					NoIguales=1;
+				
+				i++;
+				l=l-1;
+				
+			}
+			if (NoIguales==1)
+				strcpy(respuesta,"NO");
+			else
+				strcpy(respuesta,"SI");
+			
+		}
+		
+		else if(codigo == 5)
+		{
+			char NombreMayusculas[20]; 
+			strcpy(NombreMayusculas,nombre);
+			for(int i=0; NombreMayusculas[i] != '\0';i++)
+				NombreMayusculas[i] = toupper(NombreMayusculas[i]);
+			
+			sprintf(respuesta,"%s",NombreMayusculas);
+		}
+		
+		if (codigo!=0)
+		{
+			printf ("Respuesta: %s\n", respuesta);
+			// Y lo enviamos
+			write (sock_conn,respuesta, strlen(respuesta));
+		}
+	}
+	close(sock_conn);
+	printf("Conexion cerrada\n");
+	
+}
 
 int main(int argc, char *argv[])
 {
 	int sock_conn, sock_listen, ret;
 	struct sockaddr_in serv_adr;
-	char buff[512];
-	char buff2[512];
+	
 	// INICIALITZACIONS
 	// Obrim el socket
 	if ((sock_listen = socket(AF_INET, SOCK_STREAM, 0)) < 0)
@@ -35,106 +146,21 @@ int main(int argc, char *argv[])
 		printf("Error en el Listen");
 	
 	int i;
+	int sockets[100];
+	pthread_t thread[100];
+	i=0;
 	for(;;){
 		printf ("Escuchando\n");
 		
 		sock_conn = accept(sock_listen, NULL, NULL);
 		printf ("He recibido conexi?n\n");
+		sockets[i] =sock_conn;
 		//sock_conn es el socket que usaremos para este cliente
+				
+		// Crear thead y decirle lo que tiene que hacer		
+		pthread_create (&thread[i], NULL, AtenderCliente,&sockets[i]);
+		i=i+1;
 		
-		//Bucle de atencion al cliente
-		int terminal=0;
-		while (terminal ==0 )
-		{
-			// Ahora recibimos su nombre, que dejamos en buff
-			ret=read(sock_conn,buff, sizeof(buff));
-			printf ("Recibido\n");
-			
-			// Tenemos que a?adirle la marca de fin de string 
-			// para que no escriba lo que hay despues en el buffer
-			buff[ret]='\0';
-			
-			//Escribimos el nombre en la consola
-			printf ("Peticion: %s\n",buff);
-			
-			//Vamos a ver que quieren
-			char *p = strtok( buff, "/");
-			int codigo =  atoi (p);
-			char nombre[20];
-			if (codigo !=0)
-			{
-				p = strtok( NULL, "/");
-				strcpy (nombre, p);
-				printf ("Codigo: %d, Nombre: %s\n", codigo, nombre);
-			}
-			
-			if (codigo==0)
-			{
-				terminal =1;
-			}
-			
-			else if (codigo ==1) //piden la longitd del nombre
-				sprintf (buff2,"%d", strlen(nombre));
-			else if (codigo ==2 )
-				// quieren saber si el nombre es bonito
-				if((nombre[0]=='M') || (nombre[0]=='S'))
-				strcpy (buff2,"SI");
-				else
-					strcpy (buff2,"NO");
-				
-			else if(codigo==3) //Decir si es alto
-			{
-				p = strtok( NULL, "/");
-				float altura =  atof (p);
-				if (altura>1.70)
-					sprintf(buff2, "%s: eres alto", nombre);
-				else
-					sprintf(buff2, "%s: eres bajo", nombre);
-				
-			}
-			
-			else if(codigo == 4)
-			{
-				int NoIguales=0;
-				int i=0;
-				int l=strlen(nombre)-1;
-				char nombreMay[20];
-				for(i=0; nombreMay[i] != '\0';i++)
-					nombreMay[i] = toupper(nombreMay[i]);
-				while((NoIguales!=1) && (i<l))
-				{
-					if(nombreMay[i]!=nombreMay[l])
-						NoIguales=1;
-					
-					i++;
-					l=l-1;
-					
-				}
-				if (NoIguales==1)
-					strcpy(buff2,"NO");
-				else
-					strcpy(buff2,"SI");
-				
-			}
-			
-			else if(codigo == 5)
-			{
-				char NombreMayusculas[20]; 
-				strcpy(NombreMayusculas,nombre);
-				for(i=0; NombreMayusculas[i] != '\0';i++)
-					NombreMayusculas[i] = toupper(NombreMayusculas[i]);
-				
-				sprintf(buff2,"%s",NombreMayusculas);
-			}
-			
-			if (codigo!=0)
-			{
-				printf ("Respuesta: %s\n", buff2);
-				// Y lo enviamos
-				write (sock_conn,buff2, strlen(buff2));
-			}
-		}
-		close(sock_conn);
-		printf("Conexion cerrada\n");
+	
 	}
 }
